58e066883f2d5a21e6353c44c7c6d4c9f17d9d92
bhagavanyalamarthi <61960124+bhagavanyalamarthi@users.noreply.github.com>
2020-08-14 22:04:30 +0530

    Update 1.prime number.py

--- 1.prime number.py
+++ 1.prime number.py
@@ -7,3 +7,16 @@ if count==2:
      print(n, 'prime')
 else:
      print(n, 'not prime')         
+                                      
+          
+          
+          
+          MODEL 2 
+n =int(input("enter a number"))
+for num in range (1,n+1):
+      if num>1:
+           for num in range (2,num):
+                 if num%i==0:
+                    break
+           else:
+              print(num,"prime number")
